c9632c87ada5aed79ee120065fb1f9b3fe73bd09
tanejan <114944150+tanejan@users.noreply.github.com>
2026-03-06 05:16:49 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "vpc_name" {
-  value = google_compute_network.vpc.name
+  value = google_compute_network.case-study-vpc-network.name
 }
 
 output "subnet_name" {
-  value = google_compute_subnetwork.subnet.name
+  value = google_compute_subnetwork.case-study-subnetwork.name
 }
 
 output "vm_name" {
