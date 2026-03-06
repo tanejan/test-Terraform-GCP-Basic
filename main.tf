6cd0a263a386072e219447dcdd9dddba3fc6646c
tanejan <114944150+tanejan@users.noreply.github.com>
2026-03-06 05:13:29 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "google_compute_network" "case-study-vpc-network" {
+  project                                   = "neeraj"
+  name                                      = "case-study-vpc-network"
+  auto_create_subnetworks                   = false
+}
+
+resource "google_compute_subnetwork" "case-study-subnet" {
+  name          = "case-study-subnet"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.case-study-vpc-network.id
+}
+
+resource "google_compute_instance" "case-study-instance3" {
+  name         = "case-study-instance3"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params { image = "debian-cloud/debian-11" }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.case-study-subnet.id
+    access_config {}
+  }
+
+  labels = {
+    environment = var.environment
+    owner       = var.owner
+    managedby   = "terraform"
+  }
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    echo "<h1>Provisioned via Terraform</h1>" > /var/www/html/index.html
+  EOT
+}
