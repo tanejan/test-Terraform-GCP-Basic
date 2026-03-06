98616748cd2f88f68f6a7f3033d8eb6a22e4c28c
tanejan <114944150+tanejan@users.noreply.github.com>
2026-03-06 04:54:40 +0000

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "project_id"
+variable "region"
+variable "zone"
+
+variable "common_tags" {
+  type = map(string)
+  default = {
+    environment = "dev"
+    managed_by  = "terraform"
+    owner       = "platform-team"
+  }
+}
