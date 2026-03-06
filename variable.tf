6af6406b26442c63fa42709ce4be1a9502d938d2
tanejan <114944150+tanejan@users.noreply.github.com>
2026-03-06 05:37:36 +0000

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,6 +2,7 @@ variable "project_id" { type = string }
 variable "region"     { default = "us-central1" }
 variable "zone"       { default = "us-central1-a" }
 variable "environment" {default = "dev"}
+variable "owner"      {default = "neeraj"}
 
 variable "common_tags" {
   type = map(string)
