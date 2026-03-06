f10a4a6aae9879674132d11bda674d8199291199
tanejan <114944150+tanejan@users.noreply.github.com>
2026-03-06 05:15:16 +0000

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "vpc_name" {
+  value = google_compute_network.vpc.name
+}
+
+output "subnet_name" {
+  value = google_compute_subnetwork.subnet.name
+}
+
+output "vm_name" {
+  value = google_compute_instance.vm_instance.name
+}
