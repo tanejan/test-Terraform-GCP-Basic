55f1ce50cd44c208c188d0074c3a8bffa0c3a15b
tanejan <114944150+tanejan@users.noreply.github.com>
2026-03-06 05:31:39 +0000

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,12 +1,12 @@
-variable "project_id"
-variable "region"
-variable "zone"
+variable "project_id" { type = string }
+variable "region"     { default = "us-central1" }
+variable "zone"       { default = "us-central1-a" }
 
 variable "common_tags" {
   type = map(string)
   default = {
     environment = "dev"
     managed_by  = "terraform"
-    owner       = "training-team"
+    owner       = "platform-team"
   }
 }
